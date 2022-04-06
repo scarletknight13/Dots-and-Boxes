ea2000a050d464aa2b9858b91e9a5f645cb2b577
scarletknight13 <lamarbroxton@gmail.com>
2022-04-06 15:18:12 -0500

    added pseudo code

---
+++ app.js
@@ -0,0 +1,12 @@
+// pseudo code
+// int variable for player turn 0 or 1
+// int varia
+// variable array for all my boxes
+// array variable for all the sides of the boxes
+// add event listener for whenever a side gets clicked
+// add event listener to reset button that calls restart game function that will start the game from scratch 
+// when an uncolored side gets clicked check whether or not the side is the last side of a cooresponding box
+// if so color box and side to player's color and add 1 to that player's score
+// if not just change side's color to player's color
+// when all boxes are colored end game and display message with info about who won
+// call restart game function
